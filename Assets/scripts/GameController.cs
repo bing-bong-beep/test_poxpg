72609f3121ea421bdb335a02dd81700327e06328
bing-bong-beep <s30761@pjwstk.edu.pl>
2025-05-05 13:50:54 +0200

    pierwszytest

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public string Word = "after";
+    public TextMeshProUGUI MainWord;
+    public TMP_InputField InputField; 
+    public TMP_InputField SumbittedInput;
+    public TextMeshProUGUI OurInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainWord.text = Word;
+    }
+
+    public void OnInputChanged(string input)
+    {
+        OurInput.text = input;
+    }
+
+    public void OnClick()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
